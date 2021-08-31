d2f3db0b30cd54a6330bdb81e76b73831b423739
unknown <hexvillarin@gmail.com>
2021-09-01 02:54:39 +0800

    initial migration

---
+++ database/migrations/2021_08_31_181756_create_participants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('department',['hannah','a51','payout','app_request','livechat','operator','facilities','admin','nurse','spec_ops','audit','it','devs','marketing','ocb']);
+            $table->enum('shift',['morning','mid','gy']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
